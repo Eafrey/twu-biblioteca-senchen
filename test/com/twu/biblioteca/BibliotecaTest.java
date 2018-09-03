008270b47bcd747a1d99e727b52d8716ae29105c
chensen <senchen@thoughtworks.com>
2018-09-03 10:38:54 +0800

    add book list test

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class BibliotecaTest {
 
@@ -25,5 +27,15 @@ public class BibliotecaTest {
         System.setOut(new PrintStream(outputContent));
         bibliotecaApp.printWelcomeMessage();
         assertEquals("Welcome to Bangalore Public Library!\n", outputContent.toString());
+    }
+
+    @Test
+    public void shouldListBooks() {
+        final ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputContent));
+        bibliotecaApp.printBookList();
+        assertThat(outputContent.toString(), containsString("BOOK NAME"));
+        assertThat(outputContent.toString(), containsString("AUTHOR"));
+        assertThat(outputContent.toString(), containsString("PUBLISH DATE"));
     }
 }
